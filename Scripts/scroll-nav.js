2561ddcb148345afa7a55b02569a08725fd0b66e
João Vitor Bastos <156615917+bastosjoaovitor@users.noreply.github.com>
2024-07-20 19:27:03 -0300

    Linhas de separação
    Adicionado e estilizadas as linhas que separam as sections, e adaptado a rolagem do site pela aba nav para acima das linhas.

--- Scripts/scroll-nav.js
+++ Scripts/scroll-nav.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
       anchor.addEventListener('click', function(event) {
         event.preventDefault();
         const target = document.querySelector(this.getAttribute('href'));
-        const offset = 55; // Ajuste de 55px acima
+        const offset = 155; // Ajuste de 55px acima
         const bodyRect = document.body.getBoundingClientRect().top;
         const targetRect = target.getBoundingClientRect().top;
         const targetPosition = targetRect - bodyRect - offset;
